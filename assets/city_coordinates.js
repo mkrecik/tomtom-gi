1194b378e3d76b136083cc34936e6d5eb57c904f
mkrecik <99291814+mkrecik@users.noreply.github.com>
2022-12-08 10:27:47 +0100

    Update city_coordinates.js

--- assets/city_coordinates.js
+++ assets/city_coordinates.js
@@ -1,38 +1,55 @@
-var war = {lat: 52.229675, lng: 21.012230};
-const R = 6371e3; // metres
-
 function get_cord(city){
     switch(city){
-        case "war":
-            return{lat: 52.229675, lng: 21.012230};
-        case "cra":
-            return{lat: 50.064650, lng: 19.944980};
-        case "wro":
-            return {lat: 51.107885, lng: 17.038538};
-        case "poz":
-            return {lat: 52.406374, lng: 16.925168};
-        case "lod":
-            return {lat: 51.759250, lng: 19.455983};
-        case "kat":
-            return {lat: 50.264892, lng: 19.023781};
-        case "gda":
-            return {lat: 54.352025, lng: 18.646638};
-        case "ven": 
-            return {lat: 45.440847, lng: 12.315515};
-        case "mil":
-            return {lat: 45.464204, lng: 9.189982};
-        case "rom":
-            return {lat: 41.902783, lng: 12.496366};
-        case "mad":
-            return {lat: 40.416775, lng: -3.703790};
-}}
+        case "Czeladź":
+            return{lat:50.38245053885933, lng:19.077009938097365};
+        case "Gliwice":
+            return{lat:50.43871808624632, lng:18.687973999157993};
+        case "Lublin":
+            return{lat:51.303781686188216, lng:22.6599183213868};
+        case "Łódź":
+            return{lat:51.859502738472834, lng:19.433391512520288};
+        case"Poznań":
+            return{lat:52.42886777996042, lng:16.944812250093012};
+        case "PoznańWest":
+            return{lat:52.419726234508154, lng:16.780070640299336};
+        case "PruszkówI":
+            return{lat:52.18319922548114, lng:20.812755495119607};
+        case "PruszkówII":
+            return{lat:52.182049557958344, lng:20.745534043143127};
+        case "Teresin":
+            return{lat:52.227811255456714, lng:20.415527429475933};
+        case "Wrocław":
+            return{lat:51.24711033402472, lng:17.12977277282152};
+        case "WrocławWest":
+            return{lat:51.147308754165536, lng:16.93180945261786};
+        case "ParkPoznań":
+            return{lat:52.44895980409809, lng:16.887441693838113};
+        case"Gorzów":
+            return{lat:52.91851736217094, lng:15.239960257744078};
+        case"Vienna":
+            return{lat:52.17856788597538, lng:20.813442140627572};
+        case"Berlin":
+            return{lat:52.37741143164078, lng:13.160128061448656};
+        case"Niederrhein":
+            return{lat:51.780722780952416, lng:6.476814623782894};
+        case"Unna":
+            return{lat:51.9907731360565, lng:7.165018754603798};
+        case"Schalke":
+            return{lat:51.96930751626973, lng:7.098528980721935};
+        case"Bucharest":
+            return{lat:44.732465060657674, lng:25.944349185617874};
+    }
+};
 
-function get_route(city){
-    var cord = get_cord(city);
-    const φ1 = war.lat * Math.PI/180; // φ, λ in radians
-    const φ2 = cord.lat * Math.PI/180;
-    const Δφ = (cord.lat-war.lat) * Math.PI/180;
-    const Δλ = (cord.lng-war.lng) * Math.PI/180;
+const R = 6371e3; // metres
+
+function get_route(city1,city2){
+    var cord1 = get_cord(city1);
+    var cord2 = get_cord(city2);
+    const φ1 = cord1.lat * Math.PI/180; // φ, λ in radians
+    const φ2 = cord2.lat * Math.PI/180;
+    const Δφ = (cord2.lat-cord1.lat) * Math.PI/180;
+    const Δλ = (cord2.lng-cord1.lng) * Math.PI/180;
     const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ/2) * Math.sin(Δλ/2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
     const d = R * c / 1000; // in kilometres
